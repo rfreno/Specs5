f57e59cbbdb218b358d461e3a277abed7424d9b4
rfreno <wintersbek@gmail.com>
2022-11-10 18:56:49 -0700

    part 2 complete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Header from "./Components/Header";
 import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
 
+import { selectPotentials } from "./redux/slices/potentialCountriesSlice";
+import { useSelector } from "react-redux";
+
 function App() {
-    return (
-        <div className="App font-link">
-            <Header />
-            <OptionDisplay />
-        </div>
-    );
+    const potentials = useSelector(selectPotentials)
+    console.log(potentials)
+
+  return (
+    <div className="App font-link">
+      <Header />
+      <OptionDisplay />
+    </div>
+  );
 }
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import potentialCountriesReducer from "../redux/slices/potentialCountriesSlice";
+
 export default configureStore({
-  reducer: {},
+  reducer: { potentialCountries: potentialCountriesReducer },
 });
